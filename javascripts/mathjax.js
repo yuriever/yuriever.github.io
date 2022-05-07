d87e620398867167a79454a6e84fd16fdfb2c2f1
yurie <104705457+yuriever@users.noreply.github.com>
2022-05-07 21:49:47 +0800

    Deployed 443be44 with MkDocs version: 1.3.0

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -1,4 +1,17 @@
 window.MathJax = {
     loader: {load: ['[tex]/physics']},
     tex: {packages: {'[+]': ['physics']}}
-  };
+};
+
+window.MathJax = {
+  tex: {
+    macros: {
+      C: "{\\mathbb{C}}",
+      vev: ["{\\langle #1 \\rangle}", 1],
+      f: "{\\phi}"
+    },
+    enviroments: {
+      
+    }
+  }
+};
