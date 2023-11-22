e0bc8edcb9db36fccd87ae9fc0a4c9073c4e74f5
yurie <104705457+yuriever@users.noreply.github.com>
2023-11-22 12:37:40 +0800

    Deployed 8b5ecf5 with MkDocs version: 1.5.2

--- js/mathjax.js
+++ js/mathjax.js
@@ -6,6 +6,10 @@ window.MathJax = {
         // packages included
         packages: { '[+]': ['tagformat', 'configmacros', 'physics'] },
         // inline math mode delimiters
+        physics: {
+            italicdiff: true,
+            arrowdel: false
+        },
         inlineMath: [['$', '$'], ['\\(', '\\)']],
         // display math mode delimiters
         displayMath: [['$$', '$$'], ['\\[', '\\]']],
@@ -30,16 +34,12 @@ window.MathJax = {
         useLabelIds: true,
         // macros
         macros: {
-            // DONT overwrite this part
-            "ppair": ['{\\left( #1 \\right)}', "1"],
-            // DONT overwrite this part
-            //
-            //
-            // Automatically transformed from LaTeX
-            //
-            //
-            "wave": [
-                "\\widetilde{#1}",
+            "en": [
+                "(#1)",
+                "1"
+            ],
+            "check": [
+                "\\widecheck{#1}",
                 "1"
             ],
             "bar": [
@@ -50,8 +50,160 @@ window.MathJax = {
                 "\\widehat{#1}",
                 "1"
             ],
-            "case": [
-                "\\begin{cases} #1 \\end{cases}",
+            "wave": [
+                "\\widetilde{#1}",
+                "1"
+            ],
+            "poch": [
+                "(#1)_{#2}",
+                "2"
+            ],
+            "Fpq": [
+                "\\,{}_{#1}F_{#2}\\biggl(\\genfrac..{0pt}{}{#3}{#4};#5\\biggr)",
+                "5"
+            ],
+            "intt": [
+                "\\int #1\\,",
+                "1"
+            ],
+            "inttt": [
+                "\\int_{#2} #1\\,",
+                "2"
+            ],
+            "intrange": [
+                "\\int_{#2}^{#3} #1\\,",
+                "3"
+            ],
+            "ip": [
+                "(#1,#2)",
+                "2"
+            ],
+            "dual": [
+                "{#1}^\\vee",
+                "1"
+            ],
+            "lie": [
+                "\\mathfrak{#1}",
+                "1"
+            ],
+            "set": [
+                "\\{ #1 \\}",
+                "1"
+            ],
+            "linearSpan": [
+                "\\operatorname{span}\\{ #1 \\}",
+                "1"
+            ],
+            "modd": [
+                "\\, \\operatorname{mod} #1 ",
+                "1"
+            ],
+            "der": [
+                "\\frac{d}{d #1}",
+                "1"
+            ],
+            "vev": [
+                "\\langle #1 \\rangle",
+                "1"
+            ],
+            "bra": [
+                "{\\langle {#1} |}",
+                "1"
+            ],
+            "ket": [
+                "{| {#1} \\rangle}",
+                "1"
+            ],
+            "braket": [
+                "{\\langle {#1}|{#2} \\rangle}",
+                "2"
+            ],
+            "sphere": [
+                "\\operatorname{S}^{#1}",
+                "1"
+            ],
+            "hyperbolic": [
+                "\\operatorname{H}_{#1}",
+                "1"
+            ],
+            "lightcone": [
+                "\\operatorname{LC}_{#1}",
+                "1"
+            ],
+            "lightconeproj": [
+                "\\operatorname{PC}_{#1}",
+                "1"
+            ],
+            "rep": [
+                "\\mathcal{V}_{#1}",
+                "1"
+            ],
+            "urep": [
+                "\\mathcal{E}_{#1}",
+                "1"
+            ],
+            "udual": [
+                "\\wave{#1}",
+                "1"
+            ],
+            "instate": [
+                "#1_{\\operatorname{in}}",
+                "1"
+            ],
+            "outstate": [
+                "#1_{\\operatorname{out}}",
+                "1"
+            ],
+            "GeV": [
+                "\\operatorname{GeV}^{#1}",
+                "1"
+            ],
+            "normalOrder": [
+                "\\vcentcolon\\mathrel{#1}\\vcentcolon",
+                "1"
+            ],
+            "pmat": [
+                "\\pmqty{#1}",
+                "1"
+            ],
+            "Pmat": [
+                "\\Pmqty{#1}",
+                "1"
+            ],
+            "bmat": [
+                "\\bmqty{#1}",
+                "1"
+            ],
+            "vmat": [
+                "\\vmqty{#1}",
+                "1"
+            ],
+            "textInMath": [
+                "\\, \\text{ #1 }\\, ",
+                "1"
+            ],
+            "mathInTitle": [
+                "\\texorpdfstring{$#1$}{}",
+                "1"
+            ],
+            "tableVerticalSpace": [
+                "\\newcommand{\\arraystretch}{#1}",
+                "1"
+            ],
+            "ppair": [
+                "\\left(#1\\right)",
+                "1"
+            ],
+            "bpair": [
+                "\\left[#1\\right]",
+                "1"
+            ],
+            "vpair": [
+                "\\left|#1\\right|",
+                "1"
+            ],
+            "Bpair": [
+                "\\left\\{#1\\right\\}",
                 "1"
             ],
             "red": [
@@ -74,165 +226,65 @@ window.MathJax = {
                 "\\textcolor{pink}{#1}",
                 "1"
             ],
-            "notethat": [
-                "\\textcolor{red}{#1}",
-                "1"
-            ],
-            "vev": [
-                "\\langle #1 \\rangle",
-                "1"
-            ],
-            "rep": [
-                "\\cV_{#1}",
-                "1"
-            ],
-            "urep": [
-                "\\cE_{#1}",
-                "1"
-            ],
-            "urepdiscrete": [
-                "\\cD_{#1}",
-                "1"
-            ],
-            "udual": [
-                "\\wave{#1}",
-                "1"
-            ],
-            "instate": [
-                "#1_{\\operatorname{in}}",
-                "1"
-            ],
-            "outstate": [
-                "#1_{\\operatorname{out}}",
-                "1"
-            ],
-            "GeV": [
-                "\\operatorname{GeV}^{#1}",
-                "1"
-            ],
-            "intt": [
-                "\\int #1\\,",
-                "1"
-            ],
-            "inttt": [
-                "\\int_{#2} #1\\,",
-                "2"
-            ],
-            "intrange": [
-                "\\int_{#2}^{#3} #1\\,",
-                "3"
-            ],
-            "diag": [
-                "\\operatorname{diag}\\left( #1 \\right)",
-                "1"
-            ],
-            "vspan": [
-                "\\operatorname{span}\\{ #1 \\}",
-                "1"
-            ],
-            "set": [
-                "\\{ #1 \\}",
-                "1"
-            ],
-            "modd": [
-                "\\, \\operatorname{mod} #1 ",
-                "1"
-            ],
-            "dual": [
-                "{#1}^\\vee",
-                "1"
-            ],
-            "innerproductmath": [
-                "(#1,#2)",
-                "2"
-            ],
-            "innerproductphy": [
-                "#1 \\cdot #2",
-                "2"
-            ],
-            "lie": [
-                "\\mathfrak{#1}",
-                "1"
-            ],
+            "schannelcn": "$s$-道",
+            "tchannelcn": "$t$-道",
+            "uchannelcn": "$u$-道",
+            "a": "\\alpha",
+            "b": "\\beta",
+            "c": "\\chi",
+            "d": "\\delta",
+            "e": "\\epsilon",
+            "f": "\\phi",
+            "g": "\\gamma",
+            "h": "\\eta",
+            "i": "\\iota",
+            "j": "\\digamma",
+            "k": "\\kappa",
+            "l": "\\lambda",
+            "m": "\\mu",
+            "n": "\\nu",
+            "o": "\\omicron",
+            "p": "\\pi",
+            "q": "\\theta",
+            "r": "\\rho",
+            "s": "\\sigma",
+            "t": "\\tau",
+            "u": "\\upsilon",
+            "v": "\\varphi",
+            "w": "\\omega",
+            "x": "\\xi",
+            "y": "\\psi",
+            "z": "\\zeta",
             "geq": "\\geqslant",
             "leq": "\\leqslant",
-            "nn": "\\nonumber",
-            "impliesthat": "$\\implies$",
-            "peq": "\\phantom{=}",
-            "lhs": "\\text{l.h.s}",
-            "rhs": "\\text{r.h.s}",
-            "const": "\\operatorname{const}",
-            "vac": "\\ket{0}",
-            "op": "\\cO",
-            "opshadow": "\\wave{\\op}",
-            "fshadow": "\\wave{\\f}",
-            "opcomposite": "\\op_{e}",
-            "singletstructure": "r=1",
-            "gram": "G",
-            "fourpt": "\\cG",
-            "blockstripped": "g",
-            "blockdressed": "G",
-            "CPWstripped": "\\psi",
-            "CPWdressed": "\\Psi",
-            "pT": "p_{\\perp}",
-            "xT": "x_{\\perp}",
-            "disc": "\\operatorname{Disc}",
-            "fm": "\\operatorname{fm}",
-            "vp": "\\vec{p}",
-            "vx": "\\vec{x}",
-            "hb": "\\bar{h}",
-            "wbar": "\\bar{w}",
-            "zbar": "\\bar{z}",
-            "Tbar": "\\bar{T}",
-            "Lbar": "\\bar{L}",
-            "psib": "\\bar{\\psi}",
-            "Vfuture": "\\bar{V}_{+}",
-            "Vpast": "\\bar{V}_{-}",
-            "Vcausal": "\\bar{V}_{\\pm}",
             "Faa": "{}_{1}F_{1}",
             "Fab": "{}_{1}F_{2}",
             "Fba": "{}_{2}F_{1}",
             "Fcb": "{}_{3}F_{2}",
-            "half": "\\frac{1}{2}",
-            "x": "\\times",
-            "ox": "\\otimes",
-            "os": "\\oplus",
-            "w": "\\wedge",
-            "inf": "\\infty",
-            "oo": "\\infty",
-            "p": "\\partial",
-            "n": "\\nabla",
-            "sign": "\\operatorname{sgn}",
-            "EL": "\\operatorname{EL}",
-            "directint": "\\int^{\\oplus}",
-            "inverselimit": "\\varprojlim",
-            "directlimit": "\\varinjlim",
+            "residue": "\\operatorname{Res}",
+            "disc": "\\operatorname{Disc}",
+            "volume": "\\operatorname{Vol}",
+            "id": "\\operatorname{id}",
             "im": "\\operatorname{im}",
             "coim": "\\operatorname{coim}",
             "coker": "\\operatorname{coker}",
-            "opindex": "\\operatorname{index}",
-            "spec": "\\operatorname{spec}",
-            "Diff": "\\operatorname{Diff}",
-            "Hom": "\\operatorname{Hom}",
+            "dim": "\\operatorname{dim}",
+            "codim": "\\operatorname{codim}",
+            "Spec": "\\operatorname{Spec}",
             "Aut": "\\operatorname{Aut}",
             "End": "\\operatorname{End}",
-            "Out": "\\operatorname{Out}",
-            "Inn": "\\operatorname{Inn}",
-            "id": "\\operatorname{id}",
-            "ad": "\\operatorname{ad}",
-            "Ad": "\\operatorname{Ad}",
-            "codim": "\\operatorname{codim}",
-            "vol": "\\operatorname{vol}",
-            "ind": "\\operatorname{Ind}",
-            "res": "\\operatorname{Res}",
-            "rad": "\\operatorname{rad}",
-            "H": "\\mathcal{H}",
-            "M": "\\mathcal{M}",
-            "ext": "\\operatorname{Ext}",
-            "tor": "\\operatorname{Tor}",
-            "setcat": "\\operatorname{Set}",
-            "veccat": "\\operatorname{Vec}",
-            "catcat": "\\operatorname{Cat}",
+            "Outer": "\\operatorname{Out}",
+            "Inner": "\\operatorname{Inn}",
+            "adjoint": "\\operatorname{ad}",
+            "Adjoint": "\\operatorname{Ad}",
+            "induce": "\\operatorname{Ind}",
+            "restrict": "\\operatorname{Res}",
+            "inverseLimit": "\\varprojlim",
+            "directLimit": "\\varinjlim",
+            "directIntegral": "\\int^{\\oplus}",
+            "catSet": "\\operatorname{Set}",
+            "catVec": "\\operatorname{Vec}",
+            "catCat": "\\operatorname{Cat}",
             "catA": "\\mathcal{A}",
             "catB": "\\mathcal{B}",
             "catC": "\\mathcal{C}",
@@ -240,85 +292,59 @@ window.MathJax = {
             "catE": "\\mathcal{E}",
             "catF": "\\mathcal{F}",
             "catG": "\\mathcal{G}",
-            "Uvec": "\\mathbb{U}",
-            "Vvec": "\\mathbb{V}",
-            "Wvec": "\\mathbb{W}",
-            "a": "\\alpha",
-            "b": "\\beta",
-            "c": "\\chi",
-            "d": "\\delta",
-            "D": "\\Delta",
-            "e": "\\epsilon",
-            "ve": "\\varepsilon",
-            "g": "\\gamma",
-            "G": "\\Gamma",
-            "k": "\\kappa",
-            "l": "\\lambda",
-            "L": "\\Lambda",
-            "lm": "\\lambda",
-            "f": "\\phi",
-            "vf": "\\varphi",
-            "r": "\\rho",
-            "s": "\\sigma",
-            "S": "\\Sigma",
-            "o": "\\omega",
-            "O": "\\Omega",
-            "t": "\\tau",
-            "th": "\\theta",
-            "Th": "\\Theta",
-            "z": "\\zeta",
-            "R": "\\mathbb{R}",
+            "Hom": "\\operatorname{Hom}",
+            "Ext": "\\operatorname{Ext}",
+            "Tor": "\\operatorname{Tor}",
             "RR": "\\mathbb{R}",
-            "Rnot": "\\mathbb{R}^{\\neq 0}",
             "Rneq": "\\mathbb{R}^{\\neq 0}",
             "Rgeq": "\\mathbb{R}^{\\geqslant 0}",
-            "Rpositive": "\\mathbb{R}^{> 0}",
             "Rleq": "\\mathbb{R}^{\\leqslant 0}",
+            "Rpositive": "\\mathbb{R}^{> 0}",
             "Rnegative": "\\mathbb{R}^{< 0}",
-            "Z": "\\mathbb{Z}",
             "ZZ": "\\mathbb{Z}",
-            "N": "\\mathbb{N}",
             "NN": "\\mathbb{N}",
-            "Znot": "\\mathbb{Z}^{\\geqslant 0}",
             "Zneq": "\\mathbb{Z}^{\\neq 0}",
             "Zgeq": "\\mathbb{Z}^{\\geqslant 0}",
-            "Zpositive": "\\mathbb{Z}^{> 0}",
             "Zleq": "\\mathbb{Z}^{\\leqslant 0}",
+            "Zpositive": "\\mathbb{Z}^{> 0}",
             "Znegative": "\\mathbb{Z}^{< 0}",
-            "Q": "\\mathbb{Q}",
             "QQ": "\\mathbb{Q}",
-            "Qnot": "\\mathbb{Q}^{\\geqslant 0}",
             "Qneq": "\\mathbb{Q}^{\\neq 0}",
             "Qgeq": "\\mathbb{Q}^{\\geqslant 0}",
-            "Qpositive": "\\mathbb{Q}^{> 0}",
             "Qleq": "\\mathbb{Q}^{\\leqslant 0}",
+            "Qpositive": "\\mathbb{Q}^{> 0}",
             "Qnegative": "\\mathbb{Q}^{< 0}",
-            "C": "\\mathbb{C}",
-            "CC": "\\mathbb{CC}",
+            "CC": "\\mathbb{C}",
             "Cneq": "\\mathbb{C}^{\\neq 0}",
-            "Cnot": "\\mathbb{C}^{\\neq 0}",
-            "P": "\\mathbb{P}",
             "CP": "\\mathbb{CP}",
             "RP": "\\mathbb{RP}",
             "Qp": "\\mathbb{Q}_{p}",
             "Zp": "\\mathbb{Z}_{p}",
             "Up": "\\mathbb{U}_{p}",
-            "spin": "\\operatorname{Spin}",
             "glie": "\\mathfrak{g}",
-            "gllie": "\\mathfrak{gl}",
-            "sllie": "\\mathfrak{sl}",
-            "isllie": "\\mathfrak{isl}",
-            "ulie": "\\mathfrak{u}",
-            "sulie": "\\mathfrak{su}",
-            "olie": "\\mathfrak{o}",
-            "solie": "\\mathfrak{so}",
-            "isolie": "\\mathfrak{iso}",
-            "splie": "\\mathfrak{sp}",
             "nlie": "\\mathfrak{n}",
             "plie": "\\mathfrak{p}",
             "blie": "\\mathfrak{b}",
             "hlie": "\\mathfrak{h}",
             "klie": "\\mathfrak{k}",
+            "gllie": "\\mathfrak{gl}",
+            "glgroup": "\\operatorname{GL}",
+            "olie": "\\mathfrak{o}",
+            "ogroup": "\\operatorname{O}",
+            "solie": "\\mathfrak{so}",
+            "sogroup": "\\operatorname{SO}",
+            "spingroup": "\\operatorname{Spin}",
+            "isolie": "\\mathfrak{iso}",
+            "isogroup": "\\operatorname{ISO}",
+            "ulie": "\\mathfrak{u}",
+            "ugroup": "\\operatorname{U}",
+            "sulie": "\\mathfrak{su}",
+            "sugroup": "\\operatorname{SU}",
+            "sllie": "\\mathfrak{sl}",
+            "slgroup": "\\operatorname{SL}",
+            "splie": "\\mathfrak{sp}",
+            "spgroup": "\\operatorname{Sp}",
+            "Diff": "\\operatorname{Diff}",
             "cA": "\\mathcal{A}",
             "cB": "\\mathcal{B}",
             "cC": "\\mathcal{C}",
@@ -344,17 +370,49 @@ window.MathJax = {
             "cW": "\\mathcal{W}",
             "cX": "\\mathcal{X}",
             "cY": "\\mathcal{Y}",
-            "cZ": "\\mathcal{Z}"
+            "cZ": "\\mathcal{Z}",
+            "xx": "\\times",
+            "ox": "\\otimes",
+            "os": "\\oplus",
+            "ww": "\\wedge",
+            "oo": "\\infty",
+            "HH": "\\mathcal{H}",
+            "half": "\\frac{1}{2}",
+            "sign": "\\operatorname{sgn}",
+            "peq": "\\phantom{=}",
+            "lhs": "\\text{l.h.s}",
+            "rhs": "\\text{r.h.s}",
+            "const": "\\operatorname{const}",
+            "pp": "\\partial",
+            "schannel": "\\ifmmode {(s)} \\else $s$-channel \\fi",
+            "tchannel": "\\ifmmode {(t)} \\else $t$-channel \\fi",
+            "uchannel": "\\ifmmode {(u)} \\else $u$-channel \\fi",
+            "halfdim": "\\frac{d}{2}",
+            "vac": "\\ket{0}",
+            "op": "\\mathcal{O}",
+            "fourPoint": "\\cG",
+            "blockStripped": "g",
+            "blockDressed": "G",
+            "partialWaveStripped": "\\psi",
+            "partialWaveDressed": "\\Psi",
+            "fm": "\\operatorname{fm}",
+            "vp": "\\vec{p}",
+            "vx": "\\vec{x}",
+            "hb": "\\bar{h}",
+            "wbar": "\\bar{w}",
+            "zbar": "\\bar{z}",
+            "Tbar": "\\bar{T}",
+            "Lbar": "\\bar{L}",
+            "psibar": "\\bar{\\psi}",
+            "qiff": "\\qq{iff}",
+            "nn": "\\nonumber",
+            "nnn": "\\addtocounter{equation}{1}\\tag{\\theequation}"
         },
         environments: {
         },
     },
     showProcessingMessages: true,
     // package physics options
-    physics: {
-        italicdiff: false,
-        arrowdel: false
-    },
     options: {
         ignoreHtmlClass: '.',
         processHtmlClass: 'arithmatex'
