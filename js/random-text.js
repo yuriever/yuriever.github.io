8339b4fb881884fce3d28aaf592543254ab6ffbe
yurie <104705457+yuriever@users.noreply.github.com>
2023-11-22 23:17:33 +0800

    Deployed e0bc8ed with MkDocs version: 1.5.2

--- js/random-text.js
+++ js/random-text.js
@@ -20,7 +20,10 @@ function getRandomFromList(list) {
     return list[Math.floor(Math.random() * list.length)]
 };
 
-(function () {
-    document.getElementById("sora_subtitle").innerHTML = getRandomFromList(sora_subtitle_list).text
-})();
+// (function () {
+//     document.getElementById("sora_subtitle").innerHTML = getRandomFromList(sora_subtitle_list).text
+// })();
 
+(function () {
+    document.getElementById("sora_subtitle").innerHTML = sora_subtitle_list[0].text
+})();
