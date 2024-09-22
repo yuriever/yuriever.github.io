d728f6d51b67c9666376a87eb18e24baaaa0b4a3
Antoneewa <104705457+yuriever@users.noreply.github.com>
2024-09-22 15:34:56 +0800

    Deployed 50c7773 with MkDocs version: 1.6.0

--- js/mathjax.js
+++ js/mathjax.js
@@ -32,200 +32,8 @@ window.MathJax = {
 			url: (id, base) => base + '#' + encodeURIComponent(id)
 		},
 		useLabelIds: true,
-		// begin macros
+		// LaTeXTool-MathJax-Begin
 		macros: {
-			"bar":[
-				"\\overline{#1}",
-				"1"
-			],
-			"hat":[
-				"\\widehat{#1}",
-				"1"
-			],
-			"wave":[
-				"\\widetilde{#1}",
-				"1"
-			],
-			"gm":[
-				"\\Gamma[#1]",
-				"1"
-			],
-			"poch":[
-				"(#1)_{#2}",
-				"2"
-			],
-			"Fpq":[
-				"\\,{}_{#1}F_{#2}\\biggl(\\genfrac..{0pt}{}{#3}{#4};#5\\biggr)",
-				"5"
-			],
-			"multiGamma":[
-				"\\Gamma\\biggl[\\genfrac..{0pt}{}{#1}{#2}\\biggr]",
-				"2"
-			],
-			"set":[
-				"\\{ #1 \\}",
-				"1"
-			],
-			"linearspan":[
-				"\\{ #1 \\}",
-				"1"
-			],
-			"ideal":[
-				"\\langle #1 \\rangle",
-				"1"
-			],
-			"intt":[
-				"\\int #1\\,",
-				"1"
-			],
-			"inttt":[
-				"\\int_{#2} #1\\,",
-				"2"
-			],
-			"intttt":[
-				"\\int_{#2}^{#3} #1\\,",
-				"3"
-			],
-			"intrange":[
-				"\\int_{#2}^{#3} #1\\,",
-				"3"
-			],
-			"ip":[
-				"(#1,#2)",
-				"2"
-			],
-			"cat":[
-				"\\mathsf{#1}",
-				"1"
-			],
-			"lie":[
-				"\\mathfrak{#1}",
-				"1"
-			],
-			"ddx":[
-				"d^{d}x_{#1}",
-				"1"
-			],
-			"ddp":[
-				"d^{d}p_{#1}",
-				"1"
-			],
-			"xy":[
-				"x_{#1},y_{#1}",
-				"1"
-			],
-			"dxy":[
-				"dx_{#1}dy_{#1}",
-				"1"
-			],
-			"zzb":[
-				"z_{#1},\\bar{z}_{#1}",
-				"1"
-			],
-			"dzzb":[
-				"d^{2}z_{#1}",
-				"1"
-			],
-			"vev":[
-				"\\langle #1 \\rangle",
-				"1"
-			],
-			"bra":[
-				"{\\langle {#1} |}",
-				"1"
-			],
-			"ket":[
-				"{| {#1} \\rangle}",
-				"1"
-			],
-			"braket":[
-				"{\\langle {#1}|{#2} \\rangle}",
-				"2"
-			],
-			"sphere":[
-				"\\operatorname{S}^{#1}",
-				"1"
-			],
-			"hyperbolic":[
-				"\\operatorname{H}_{#1}",
-				"1"
-			],
-			"lightcone":[
-				"\\operatorname{LC}_{#1}",
-				"1"
-			],
-			"lightconeproj":[
-				"\\operatorname{PC}_{#1}",
-				"1"
-			],
-			"ads":[
-				"\\operatorname{AdS}_{#1}",
-				"1"
-			],
-			"eads":[
-				"\\operatorname{EAdS}_{#1}",
-				"1"
-			],
-			"ds":[
-				"\\operatorname{dS}_{#1}",
-				"1"
-			],
-			"kds":[
-				"\\operatorname{KdS}_{#1}",
-				"1"
-			],
-			"rep":[
-				"\\mathcal{V}_{#1}",
-				"1"
-			],
-			"urep":[
-				"\\mathcal{E}_{#1}",
-				"1"
-			],
-			"unitaryDual":[
-				"\\wave{#1}",
-				"1"
-			],
-			"normalOrder":[
-				":\\mathrel{#1}:",
-				"1"
-			],
-			"pmat":[
-				"\\pmqty{#1}",
-				"1"
-			],
-			"bmat":[
-				"\\bmqty{#1}",
-				"1"
-			],
-			"cmat":[
-				"\\mqty{#1}",
-				"1"
-			],
-			"vmat":[
-				"\\vmqty{#1}",
-				"1"
-			],
-			"textInMath":[
-				"\\, \\text{ #1 }\\, ",
-				"1"
-			],
-			"ppair":[
-				"\\left(#1\\right)",
-				"1"
-			],
-			"bpair":[
-				"\\left[#1\\right]",
-				"1"
-			],
-			"cpair":[
-				"\\left\\{#1\\right\\}",
-				"1"
-			],
-			"vpair":[
-				"\\left|#1\\right|",
-				"1"
-			],
 			"a":"\\alpha",
 			"b":"\\beta",
 			"c":"\\chi",
@@ -252,6 +60,18 @@ window.MathJax = {
 			"x":"\\xi",
 			"y":"\\psi",
 			"z":"\\zeta",
+			"bar":[
+				"\\overline{#1}",
+				1
+			],
+			"hat":[
+				"\\widehat{#1}",
+				1
+			],
+			"wave":[
+				"\\widetilde{#1}",
+				1
+			],
 			"eq":"=",
 			"eqq":"\\equiv",
 			"geq":"\\geqslant",
@@ -259,10 +79,26 @@ window.MathJax = {
 			"leq":"\\leqslant",
 			"le":"\\leqslant",
 			"peq":"\\phantom{{}={}}",
+			"gm":[
+				"\\Gamma[#1]",
+				1
+			],
+			"poch":[
+				"(#1)_{#2}",
+				2
+			],
 			"Faa":"{}_{1}F_{1}",
 			"Fab":"{}_{1}F_{2}",
 			"Fba":"{}_{2}F_{1}",
 			"Fcb":"{}_{3}F_{2}",
+			"Fpq":[
+				"\\,{}_{#1}F_{#2}\\biggl(\\genfrac..{0pt}{}{#3}{#4};#5\\biggr)",
+				5
+			],
+			"multiGamma":[
+				"\\Gamma\\biggl[\\genfrac..{0pt}{}{#1}{#2}\\biggr]",
+				2
+			],
 			"xx":"\\times",
 			"ox":"\\otimes",
 			"oxbig":"\\bigotimes",
@@ -274,6 +110,18 @@ window.MathJax = {
 			"HH":"\\mathcal{H}",
 			"half":"\\frac{1}{2}",
 			"sign":"\\operatorname{sgn}",
+			"set":[
+				"\\{ #1 \\}",
+				1
+			],
+			"linearspan":[
+				"\\{ #1 \\}",
+				1
+			],
+			"ideal":[
+				"\\langle #1 \\rangle",
+				1
+			],
 			"lhs":"\\text{l.h.s}",
 			"rhs":"\\text{r.h.s}",
 			"const":"\\mathrm{const.}",
@@ -283,10 +131,30 @@ window.MathJax = {
 			"Re":"\\operatorname{Re}",
 			"Im":"\\operatorname{Im}",
 			"volume":"\\operatorname{Vol}",
+			"intt":[
+				"\\int #1\\,",
+				1
+			],
+			"inttt":[
+				"\\int_{#2} #1\\,",
+				2
+			],
+			"intttt":[
+				"\\int_{#2}^{#3} #1\\,",
+				3
+			],
+			"intrange":[
+				"\\int_{#2}^{#3} #1\\,",
+				3
+			],
 			"residue":"\\operatorname{Res}",
 			"disc":"\\operatorname{Disc}",
-			"tp":"{\\scriptscriptstyle{\\mathsf{T}}}",
+			"tp":"{\\scriptscriptstyle \\mathsf{T} }",
 			"dual":"\\prime",
+			"ip":[
+				"(#1,#2)",
+				2
+			],
 			"inj":"\\rightarrowtail",
 			"surj":"\\twoheadrightarrow",
 			"im":"\\operatorname{im}",
@@ -305,6 +173,10 @@ window.MathJax = {
 			"inverseLimit":"\\varprojlim",
 			"directLimit":"\\varinjlim",
 			"directIntegral":"\\int^{\\oplus}",
+			"cat":[
+				"\\mathsf{#1}",
+				1
+			],
 			"catSet":"\\mathsf{Set}",
 			"catVec":"\\mathsf{Vec}",
 			"catCat":"\\mathsf{Cat}",
@@ -344,6 +216,10 @@ window.MathJax = {
 			"Qp":"\\mathbb{Q}_{p}",
 			"Zp":"\\mathbb{Z}_{p}",
 			"Up":"\\mathbb{U}_{p}",
+			"lie":[
+				"\\mathfrak{#1}",
+				1
+			],
 			"glie":"\\mathfrak{g}",
 			"nlie":"\\mathfrak{n}",
 			"plie":"\\mathfrak{p}",
@@ -394,9 +270,49 @@ window.MathJax = {
 			"cX":"\\mathcal{X}",
 			"cY":"\\mathcal{Y}",
 			"cZ":"\\mathcal{Z}",
-			"shadow":"\\cS",
-			"lightray":"\\cL",
+			"ddx":[
+				"d^{d}x_{#1}",
+				1
+			],
+			"ddp":[
+				"\\frac{d^{d}p_{#1}}{(2\\pi)^{d}}",
+				1
+			],
+			"xy":[
+				"x_{#1},y_{#1}",
+				1
+			],
+			"dxy":[
+				"dx_{#1}dy_{#1}",
+				1
+			],
+			"zzb":[
+				"z_{#1},\\bar{z}_{#1}",
+				1
+			],
+			"dzzb":[
+				"d^{2}z_{#1}",
+				1
+			],
+			"shadow":"\\mathcal{S}",
+			"lightray":"\\mathcal{L}",
 			"vac":"\\ket{0}",
+			"vev":[
+				"\\langle #1 \\rangle",
+				1
+			],
+			"bra":[
+				"\\langle #1 |",
+				1
+			],
+			"ket":[
+				"| #1 \\rangle",
+				1
+			],
+			"braket":[
+				"\\langle #1|#2 \\rangle",
+				2
+			],
 			"op":"\\mathcal{O}",
 			"CF":"\\mathcal{G}",
 			"CFStripped":"\\mathcal{G}",
@@ -406,7 +322,51 @@ window.MathJax = {
 			"CPW":"\\Psi",
 			"CPWStripped":"\\Psi",
 			"CPWC":"\\rho",
-			"CPWCWithS":"\\wave{\\rho}",
+			"CPWCWithS":"\\hat{\\rho}",
+			"sphere":[
+				"\\operatorname{S}^{#1}",
+				1
+			],
+			"hyperbolic":[
+				"\\operatorname{H}_{#1}",
+				1
+			],
+			"lightcone":[
+				"\\operatorname{LC}_{#1}",
+				1
+			],
+			"lightconeproj":[
+				"\\operatorname{PC}_{#1}",
+				1
+			],
+			"ads":[
+				"\\operatorname{AdS}_{#1}",
+				1
+			],
+			"eads":[
+				"\\operatorname{EAdS}_{#1}",
+				1
+			],
+			"ds":[
+				"\\operatorname{dS}_{#1}",
+				1
+			],
+			"kds":[
+				"\\operatorname{KdS}_{#1}",
+				1
+			],
+			"rep":[
+				"\\mathcal{V}_{#1}",
+				1
+			],
+			"urep":[
+				"\\mathcal{E}_{#1}",
+				1
+			],
+			"unitaryDual":[
+				"\\widetilde{#1}",
+				1
+			],
 			"vx":"\\vec{x}",
 			"vy":"\\vec{y}",
 			"vp":"\\vec{p}",
@@ -419,14 +379,81 @@ window.MathJax = {
 			"Lb":"\\bar{L}",
 			"psib":"\\bar{\\psi}",
 			"chib":"\\bar{\\chi}",
+			"normalOrder":[
+				":\\mathrel{#1}:",
+				1
+			],
 			"halfdim":"\\frac{d}{2}",
 			"fm":"\\operatorname{fm}",
 			"GeV":"\\operatorname{GeV}",
+			"eqqq":":=",
+			"id":"\\mathbf{1}",
+			"schannel":"(s)",
+			"tchannel":"(t)",
+			"uchannel":"(u)",
+			"pmat":[
+				"\\pmqty{#1}",
+				1
+			],
+			"bmat":[
+				"\\bmqty{#1}",
+				1
+			],
+			"cmat":[
+				"\\mqty{#1}",
+				1
+			],
+			"vmat":[
+				"\\vmqty{#1}",
+				1
+			],
+			"textInMath":[
+				"\\, \\text{ #1 }\\,",
+				1
+			],
 			"nn":"\\nonumber",
-			"id":"\\operatorname{id}",
-			"eqqq":":="
+			"ppair":[
+				"\\left(#1\\right)",
+				1
+			],
+			"bpair":[
+				"\\left[#1\\right]",
+				1
+			],
+			"cpair":[
+				"\\left\\{#1\\right\\}",
+				1
+			],
+			"vpair":[
+				"\\left|#1\\right|",
+				1
+			],
+			"mathred":[
+				"{\\color{red} #1}",
+				1
+			],
+			"mathblue":[
+				"{\\color{blue} #1}",
+				1
+			],
+			"mathgreen":[
+				"{\\color{orange} #1}",
+				1
+			],
+			"mathorange":[
+				"{\\color{orange} #1}",
+				1
+			],
+			"mathpurple":[
+				"{\\color{purple} #1}",
+				1
+			],
+			"mathpink":[
+				"{\\color{pink} #1}",
+				1
+			]
 		},
-		// end macros
+		// LaTeXTool-MathJax-End
 		environments: {
 		}
 	},
